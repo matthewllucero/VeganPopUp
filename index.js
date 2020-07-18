16d2982d33cf2a342494dc69133fec29fb111fbf
Matthew Lucero <matthew.lucero92@yahoo.com>
2020-07-17 22:27:58 -0700

    email

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ var button = document.getElementById("submitButton");
 var ans = document.getElementById("ans");
 
 button.addEventListener("click", function(){
-	ans.innerHTML = "answer";
+	ans.innerHTML = "sent";
+	
+	Email.send({
+    	SecureToken : config.SECURE_TOKEN,
+    	To : config.EMAIL,
+    	From : config.EMAIL,
+    	Subject : "This is the subject",
+    	Body : "And this is the body"
+	}).then(
+  		message => alert(message)
+	);
+	
 });
